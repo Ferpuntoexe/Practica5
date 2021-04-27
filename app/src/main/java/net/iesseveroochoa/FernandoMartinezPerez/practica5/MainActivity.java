fb200c257c8208953b0fbd6dbd4adaf4deadad6f
kurur <kururu.fm@gmail.com>
2021-04-27 22:23:14 +0200

    mejora

--- app/src/main/java/net/iesseveroochoa/FernandoMartinezPerez/practica5/MainActivity.java
+++ app/src/main/java/net/iesseveroochoa/FernandoMartinezPerez/practica5/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.*;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it ispresent.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
